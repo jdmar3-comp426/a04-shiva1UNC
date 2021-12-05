c619c4ce70b0288de4e35d7163a42ae05c1c7fcd
shiva1UNC <shiva1@live.unc.edu>
2021-12-04 22:59:37 -0500

    changed get user

--- server.js
+++ server.js
@@ -36,9 +36,9 @@ app.get("/app/users", (req, res) => {
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
 app.get("/app/user/:id", (req, res) => {
-	const stmt = db.prepare('SELECT * FROM userinfo WHERE id = ?');
+	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = ?");
 	const user = stmt.get(req.params.id);
-	res.status(200).json(stmt);
+	res.status(200).json(user);
 });
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/update/user/:id", (req, res) => {
